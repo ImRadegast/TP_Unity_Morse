a136f7dd1ce67d6b0c2506c9659747a383350335
DymitriChabert <dymitri.chabert@yahoo.fr>
2022-12-09 15:04:47 +0100

    Update textColorUpdate.cs

--- Assets/Script/textColorUpdate.cs
+++ Assets/Script/textColorUpdate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,23 +7,35 @@ using UnityEngine.UI;
 
 public class textColorUpdate : MonoBehaviour
 {
-    [SerializeField] private Text m_TextMeshPro;
-    [SerializeField] private Color baseColor;
-    [SerializeField] private Color ValidateColor;
-    [SerializeField] private GameObject Entity;
+     public Text m_TextMeshPro;
+     private Color baseColor = Color.white;
+     private Color ValidateColor = Color.cyan;
+     private GameObject Entity;
     // Start is called before the first frame update
 
-   public void UpdateColor()
+
+    public void UpdateStart()
     {
+        if(m_TextMeshPro != null && Entity != null)
+        {
+            UpdateColor(m_TextMeshPro, baseColor, ValidateColor, Entity);
+        }
+    }
+    public void UpdateColor(Text m_TextMeshPro, Color baseColor, Color ValidateColor, GameObject Entity)
+    {
+        
+
         //Parcour touts les Enfants de child
         for (int i = 0; i < Entity.transform.childCount; i++)
         {
             GameObject theHorseChild = Entity.transform.GetChild(i).transform.Find("text").gameObject;
           
             string text = theHorseChild.GetComponent<TextMeshPro>().text;
-            
+
             // Comment fire la correctin de caractere ?
             // achaque espace supprimer la partit ecrite et l'ecrire dans un text mesh au dessus ?
+            Console.WriteLine(i + "/" + Entity.transform.childCount);
+            
         }
        
     }
